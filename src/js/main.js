3a3b5529466deda9c2d2090c5e091f3e18638beb
ThumbSnail <thumbsnail@zoho.com>
2015-11-07 15:22:01 -0800

    Add:  Simple Google Map

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+
+/* Uncomment for map
+var gMap;
+
+function initMap() {
+	gMap = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 44.211551, lng: -120.572759},		//center of Oregon
+		zoom: 7											//zoom: less 0 -- 18 more
+	});
+}
+
+$(document).ready(initMap);
+*/
