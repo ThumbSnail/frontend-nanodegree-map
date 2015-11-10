3a1d2aaaf627b3cd626198f3c88e70846a8f4567
ThumbSnail <thumbsnail@zoho.com>
2015-11-09 16:50:32 -0800

    Add:  Site fully loads (no more testing buttons), initial Knockout.js stuff
    -site loads either from localStorage or by making a request for data

--- src/js/main.js
+++ src/js/main.js
@@ -1,158 +1,86 @@
+//Order of all this stuff...
+/*
+ -basic map can fire right away  (can stick the initMap as a callback on the google script)
+ -wiki / localStorage data grab == can fire right away
 
-//Uncomment for map
+ -**creating the map markers == google script must be loaded AND wiki/localStorage must be done
+  -then place the map markers / create the infoWindow
+   --^Really, the list shouldn't be clickable until the map markers are created (AND placed)
+
+ -calls to weather data upon click.
+
+
+//interesting, has you create deferreds:
+http://stackoverflow.com/questions/15594567/how-to-use-jquery-when-done
+*/
+
+//That bug on a local machine is bad:  where it fires off 25 get requests to file://wiki-image-site
+//really slows things down.  Hopefully it won't do that on github pages... (since it's http:)
+
+// !!!! TODO:  Need to test what happens with no localStorage AND no internet/firewall blocks wiki.
+  //jsonp doesn't work with .fail right?  So a .fail() after the .done() of getParksData won't
+  //ever trigger?  Is that correct?
+
 var gMap;
+var arrParks = [];
+var arrMarkers = [];  //or should these markers be added as a property of the Park object?
+var activeMarkerIndex = -1;
+var infoWindow;
+
+$(document).ready(function() {
+	//initMap();
+	if (localStorage.getItem('arrParks')) {
+		arrParks = JSON.parse(localStorage.getItem('arrParks'));
+		//createMapMarkers();
+		//placeMapMarkers();
+		//setupMarkerFeedback();
+		initViewModel();
+	}
+	else {
+		getParksData().done(createMapMarkers, placeMapMarkers, setupMarkerFeedback,
+			initViewModel);
+	}
+});
+
 
 function initMap() {
 	gMap = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 39.768491, lng: -86.157679},		//center of Indiana
 		zoom: 7											//zoom: less 0 -- 18 more
 	});
+
+	infoWindow = new google.maps.InfoWindow();  //just one so that only one can be open at a time
+	google.maps.event.addListener(infoWindow,'closeclick', function() {
+		arrMarkers[activeMarkerIndex].setAnimation(null);
+		activeMarkerIndex = -1;
+	});
 }
 
+function getParksData() {
+	var wikiLink = 'http://en.wikipedia.org/w/api.php?action=parse&section=1&prop=text'
+	+ '&page=List_of_Indiana_state_parks&format=json&callback=?';
 
-var arrParks;
-var arrMarkers = [];  //or should these markers be added as a property of the Park object?
-var activeMarkerIndex = -1;
-var infoWindow = new google.maps.InfoWindow();  //just one so that only one can be open at a time
-google.maps.event.addListener(infoWindow,'closeclick',function() {
-	arrMarkers[activeMarkerIndex].setAnimation(null);
-	activeMarkerIndex = -1;
-});
-
-$('#btn-create').on('click', createMapMarkers);
-$('#btn-place').on('click', placeMapMarkers);
-
-function createMapMarkers() {
-	var len = arrParks.length;
-	for (var i = 0; i < len; i++) {
-		var marker = new google.maps.Marker({
-		    position: arrParks[i].coords,
-		    title: arrParks[i].name
-		});
-
-		marker.id = i;
-
-		arrMarkers.push(marker);
-	}
+	return $.getJSON(wikiLink, wikiCallback);
 }
-
-function placeMapMarkers() {
-	var len = arrMarkers.length;
-	for (var i = 0; i < len; i++) {
-		arrMarkers[i].setMap(gMap);
-	}
-
-	setupMarkerFeedback();
-}
-
-// TODO:  I feel like only one marker should animate at a time.  I suppose a solution
-//is to give each marker an index/id.  When clicked, update the activeMarkerIndex.
-//then, disable that 
-
-//adapted from:  https://developers.google.com/maps/documentation/javascript/markers
-function setupMarkerFeedback() {
-	var len = arrMarkers.length;
-	for (var i = 0; i < len; i++) {
-		arrMarkers[i].addListener('click', function() {
-			if (this.getAnimation()) {  //then you've clicked the same marker
-				this.setAnimation(null);
-				infoWindow.close();
-				activeMarkerIndex = -1;
-			}
-			else {
-				this.setAnimation(google.maps.Animation.BOUNCE);
-				infoWindow.setContent(getDataForInfoWindow(this.id));
-				infoWindow.open(gMap, this);
-				if (activeMarkerIndex > -1) {  //a previous marker is still bouncing
-					arrMarkers[activeMarkerIndex].setAnimation(null);
-				}
-				activeMarkerIndex = this.id;
-			}
-		});
-	}
-}
-
-function getDataForInfoWindow(index) {
-	var title = '<h1 class="info-title"><a href="https://en.wikipedia.org/wiki/'
-		+ arrParks[index].name + ' State Park" target="_blank">' + arrParks[index].name
-		+ '</a></h1>';
-	var img = arrParks[index].img ? '<div class="info"><img class="info-img" src="'
-		+ arrParks[index].img + '"></div>' : '';
-	var desc = arrParks[index].desc ? '<p>' + arrParks[index].desc + '</p>' : '';
-	var wikiEnd = 'via <a href="https://en.wikipedia.org/wiki/'
-		+ 'List_of_Indiana_state_parks" target="_blank">Wikipedia</a></p>';
-	var attrWeather = '<p class="attribution">Weather data via OpenWeatherMap</p>';
-
-	var wikiStart = '';
-	if (img && desc) {
-		wikiStart = '<p class="attribution">Photo and fact ';
-	}
-	else {
-		if (img) {
-			wikiStart = '<p class="attribution">Photo ';
-		}
-		else if (desc) {
-			wikiStart = '<p class="attribution">Fact ';
-		}
-	}
-
-	if (!wikiStart) {
-		wikiEnd = '';
-	}
-
-
-	var HTML = title + img + desc + wikiStart + wikiEnd + attrWeather;
-
-	return HTML;
-}
-
-$(document).ready(function() {
-	arrParks = JSON.parse(localStorage.getItem('arrParks'));
-	initMap();
-});
-
-/*
-$(document).ready(function() {
-	//Link to a Wiki page containing a table that lists all of Indiana's State Parks (and their coordinates)
-	var wikiLink = 'http://en.wikipedia.org/w/api.php?action=parse&section=1&prop=text&page=List_of_Indiana_state_parks&format=json&callback=?';
-
-	
-	//$.getJSON(wikiLink, wikiCallback);
-	  //since^ I'm currently saving the data in localStorage, save on API calls.
-
-
-	//test retrieval of arrParks in localStorage
-	arrParks = JSON.parse(localStorage.getItem('arrParks'));
-
-	console.log(arrParks);  //cool.  now test putting the markers on the map?
-
-});
-*/
 
 function wikiCallback(data) {
-	if (data.parse.text) {  //Success!
-		var wikiHTML = data.parse.text['*'];  //Grab the HTML
-		localStorage.setItem('wikiHTML', wikiHTML);  // TODO: won't be saving this; instead will be saving the model data
+	if (data.parse.text) {
+		parseData(data.parse.text['*']);
 	}
 	else {  // TODO:  Better handling
 		console.log("Error in getting Wiki data.");
 	}	
 }
 
-var Park = function(name, img, desc, coords) {
+var Park = function(name, img, desc, coords, id) {
 	this.name = name;
 	this.img = img;
 	this.desc = desc;
 	this.coords = coords;
+	this.id = id;
 };
 
-var arrParks = [];
-
-
-$('#btn').on('click', function() {
-
-	var wikiHTML = localStorage.getItem('wikiHTML');
-
+function parseData(wikiHTML) {
 	/* Grab the data in the desired columns from the table on the Wiki page */
 	var arrNames = tableColumnToArray(1, false);
 	var arrImages = tableColumnToArray(2, true);
@@ -163,10 +91,10 @@ $('#btn').on('click', function() {
 	arrCoords = extractCoords(arrCoords);
 	arrDescs = trimDescs(arrDescs);
 
-	// TODO:  Turn this into model data.
+	/* Create the Model with this parks data */
 	var numParks = arrNames.length;
 	for (var i = 0; i < numParks; i++) {
-		var park = new Park(arrNames[i], arrImages[i], arrDescs[i], arrCoords[i]);
+		var park = new Park(arrNames[i], arrImages[i], arrDescs[i], arrCoords[i], i);
 
 		arrParks.push(park);
 	}
@@ -176,9 +104,7 @@ $('#btn').on('click', function() {
 	 *  http://stackoverflow.com/questions/3357553/how-to-store-an-array-in-localstorage
 	*/
 
-	//huh, is this a SUPER slow action?  [seems like it takes forever...]
 	localStorage.setItem('arrParks', JSON.stringify(arrParks));
-
 
 
 	/*  function tableColumnToArray(colIndex, containsImages)
@@ -269,4 +195,107 @@ $('#btn').on('click', function() {
 
 		return array;
 	}
-});
+}
+
+
+function createMapMarkers() {
+	var len = arrParks.length;
+	for (var i = 0; i < len; i++) {
+		var marker = new google.maps.Marker({
+		    position: arrParks[i].coords,
+		    title: arrParks[i].name
+		});
+
+		marker.id = i;
+
+		arrMarkers.push(marker);
+	}
+}
+
+function placeMapMarkers() {
+	var len = arrMarkers.length;
+	for (var i = 0; i < len; i++) {
+		arrMarkers[i].setMap(gMap);
+	}
+}
+
+// TODO:  I feel like only one marker should animate at a time.  I suppose a solution
+//is to give each marker an index/id.  When clicked, update the activeMarkerIndex.
+//then, disable that 
+
+//huh, could you use $(arrMarker[i]).trigger('click') for when a list-view item is clicked?
+  //yeah and you can maybe use jQuery's .index() to grab the value needed for i
+  //https://api.jquery.com/index/
+
+  //well, I think you're at the spot where you need to do knockoutjs
+
+//adapted from:  https://developers.google.com/maps/documentation/javascript/markers
+function setupMarkerFeedback() {
+	var len = arrMarkers.length;
+	for (var i = 0; i < len; i++) {
+		arrMarkers[i].addListener('click', function() {
+			if (this.getAnimation()) {  //then you've clicked the same marker
+				this.setAnimation(null);
+				infoWindow.close();
+				activeMarkerIndex = -1;
+			}
+			else {
+				this.setAnimation(google.maps.Animation.BOUNCE);
+				infoWindow.setContent(getDataForInfoWindow(this.id));
+				infoWindow.open(gMap, this);
+				if (activeMarkerIndex > -1) {  //a previous marker is still bouncing
+					arrMarkers[activeMarkerIndex].setAnimation(null);
+				}
+				activeMarkerIndex = this.id;
+			}
+		});
+	}
+}
+
+function getDataForInfoWindow(index) {
+	var title = '<h1 class="info-title"><a href="https://en.wikipedia.org/wiki/'
+		+ arrParks[index].name + ' State Park" target="_blank">' + arrParks[index].name
+		+ '</a></h1>';
+	var img = arrParks[index].img ? '<div class="info"><img class="info-img" src="'
+		+ arrParks[index].img + '"></div>' : '';
+	var desc = arrParks[index].desc ? '<p>' + arrParks[index].desc + '</p>' : '';
+	var wikiEnd = 'via <a href="https://en.wikipedia.org/wiki/'
+		+ 'List_of_Indiana_state_parks" target="_blank">Wikipedia</a></p>';
+	var attrWeather = '<p class="attribution">Weather data via OpenWeatherMap</p>';
+
+	var wikiStart = '';
+	if (img && desc) {
+		wikiStart = '<p class="attribution">Photo and fact ';
+	}
+	else {
+		if (img) {
+			wikiStart = '<p class="attribution">Photo ';
+		}
+		else if (desc) {
+			wikiStart = '<p class="attribution">Fact ';
+		}
+	}
+
+	if (!wikiStart) {
+		wikiEnd = '';
+	}
+
+
+	var HTML = title + img + desc + wikiStart + wikiEnd + attrWeather;
+
+	return HTML;
+}
+
+function ViewModel() {
+	var self = this;
+
+	self.parkList = ko.observableArray(arrParks);
+
+	self.mimicMarkerClick = function() {
+		console.log("You clicked me.");
+	}
+}
+
+function initViewModel() {
+	ko.applyBindings(new ViewModel());
+}
